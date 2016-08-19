5bdd5584d859f35f83e5f69c232a5b47309e968b
unknown <poojav@poojav.cybage.com>
2016-08-19 13:57:34 +0530

    19 august 2016

---
+++ Navigation/js/hideMenu.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+$(function(){
+  $('#showPhoneNav').click(function() {
+    $("#myNav").slideToggle('normal',function(){ //Shows Nav area
+	if ($('#myNav').is(':visible')) {
+		$('#showPhoneNav').text('Hide Navigation');
+		/*$('#showPhoneNav').animate({
+          backgroundColor: "#aa0000",
+          color: "#fff",
+          width: 500
+        }, 1000 );*/
+	} else {
+		$('#showPhoneNav').text('Show Navigation');
+	} //end of if
+	}); //end of slidetoggle
+  }); //end of myNav
+}); //end showPhoneNav
+}); //end of ready
